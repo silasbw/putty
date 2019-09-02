1e49d4a2e7218fb83b843aa56e9c88a5a0714d7f
Silas Boyd-Wickizer <silasbw@gmail.com>
2019-09-02 15:59:03 -0700

    Read JSON Patch to apply from env JSON_PATCH

--- init.sh
+++ init.sh
@@ -3,13 +3,15 @@
 set -e
 
 echo "[Uninstalling]"
-kubectl delete ValidatingWebhookConfiguration/putty 2> /dev/null || /bin/true
+kubectl delete MutatingWebhookConfiguration/putty 2> /dev/null || /bin/true
 kubectl delete svc/putty 2> /dev/null || /bin/true
 kubectl delete deploy/putty 2> /dev/null || /bin/true
+kubectl delete cm/putty 2> /dev/null || /bin/true
 
 echo "[Installing]"
+kubectl apply -f example/putty-cm.yaml
 kubectl apply -f example/putty-deploy.yaml
 kubectl apply -f example/putty-svc.yaml
 sleep 5
-kubectl apply -f example/validating-webhook-configuration.yaml
+kubectl apply -f example/mutating-webhook-configuration.yaml
 echo "[Done]"
